f7fa6369179348d9c387dd9fa736b85d805e77c9
Sara <sara@Saras-MacBook-Pro.local>
2020-04-02 20:18:52 +0200

    fetching weather from api

--- App.js
+++ App.js
@@ -2,11 +2,13 @@ import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import styled from "styled-components"
 import MyButton from './components/Button';
+import Weather from './components/Weather';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
+      <Weather />
     <MyButton />
         
     </View>
---
+++ components/Weather.js
@@ -0,0 +1,35 @@
+import React, { useState, useEffect } from "react";
+import { StyleSheet, Text, View } from 'react-native';
+
+// const secondApi = `https://api.openweathermap.org/data/2.5/forecast?q=${cityApi}&appid=2b9468766d0e54560c7e599762d2e80b`
+export default function Weather() {
+
+    const [forecast, setForecast] = useState([])
+    const [done, setDone] = useState(false)
+    const weatherapi = "https://api.openweathermap.org/data/2.5/forecast?q=Stockholm&appid=2b9468766d0e54560c7e599762d2e80b"
+
+    useEffect(()=>{
+        fetch(weatherapi)
+        .then(res => res.json())
+        .then(json => {
+            setForecast(json.list)
+            setDone(true)
+            console.log("list",json.list)
+            alert(json.list[1].weather)})
+    },[])
+
+
+    return (
+        <View>
+           
+        <Text>from my component!</Text>
+        {done && forecast.map((time, index, array)=>{
+            if ((time.dt_txt).split(' ').includes("12:00:00")){
+                return(
+            <Text> {time.dt_txt} </Text>)}
+        })}
+        </View>
+    );
+  }
+  
+ 
